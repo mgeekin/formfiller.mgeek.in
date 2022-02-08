4c7ef274c3fdc8429bb6c5af37e34429ab058a32
prateekrajgautam <prateekrajgautam@gmail.com>
2022-02-08 22:36:03 +0530

    update website

---
+++ demoform/script.js
@@ -0,0 +1,116 @@
+
+function gen(elementtype, idin, htmlin, classin) {
+    var element = document.createElement(elementtype);
+    if (idin != undefined) {
+        element.id = idin;
+    }
+    if (htmlin != undefined) {
+        if (htmlin.nodeName === undefined) {
+            element.innerHTML = htmlin;
+            element.value = htmlin;
+        };
+        if (htmlin.nodeName != undefined) {
+            element.append(htmlin);
+            console.log(htmlin);
+            console.log(htmlin.nodeName);
+        };
+    }
+
+    if (classin != undefined) {
+        element.classList.add(classin);
+    }
+    return element;
+};
+
+const body = document.querySelector("app");
+const header = gen("div", "header");
+header.append(gen("h2", "appTitle", "Form Helper"));
+const main = gen("div", "main");
+const footer = gen("div", "footer", `<i>by Dr. Prateek Raj Gautam</i>`);
+
+body.append(header);
+header.append(gen("p", "hint", "demo form to test working of FormHelper no data is saved here"))
+body.append(main);
+//body.append(footer);
+
+
+//main.append(gen("H1", "Heading1", "Heading 1"));
+
+
+//a.append(r)
+var formfields = [
+    {
+        "heading": "Basic",
+        "data": ['Full name', 'First name', 'Middle name', "Last name",]
+    },
+    {
+        "heading": "Education",
+        "data": ['degee', 'college', 'year', "percentage",]
+    },
+    {
+        "heading": "Experience",
+        "data": ['Designation', 'Institution', 'Start year', "End year",]
+    },
+    {
+        "heading": "Publication 1",
+        "data": ['Title', 'Journal', 'ISSN', "Year", "Authors", "Number of authors"]
+    },
+    {
+        "heading": "Publication 2",
+        "data": ['Title', 'Journal', 'ISSN', "Year", "Authors", "Number of authors"]
+    },
+
+    {
+        "heading": "Publication 3",
+        "data": ['Title', 'Journal', 'ISSN', "Year", "Authors", "Number of authors"]
+    }
+];
+
+
+for (var i = 0; i < formfields.length; i++) {
+    var heading = formfields[i].heading;
+    var datafield = formfields[i].data;
+    console.log(datafield)
+    main.append(gen("div", `div${heading}`));
+
+    document.getElementById(`div${heading}`).append(gen("h1", `heading${i}`, `${heading}`));
+    document.getElementById(`div${heading}`).append(gen("table", `table${i}`));
+    for (var j = 0; j < datafield.length; j++) {
+        var dataentry = datafield[j];
+        console.log(dataentry);
+        var row = gen("tr", `formrow${i}${j}`, "", "row");
+        var ltext = gen("td", `formltd${i}${j}`, dataentry, "tdl");
+        var rinput = gen("td", `formrtd${i}${j} `, gen("input", `datafield[j]`, "", "input"));
+        //document.getElementById(`$forminput${i} `).value = `$forminput${i} `;
+        document.getElementById(`table${i}`).append(row);
+        row.append(ltext, rinput);
+    }
+    //document.getElementById(`lia${ i } `).href = "/";
+};
+
+
+main.append(gen("div", "resetAll", "Reset All", "button"));
+resetAll.addEventListener("click", cleaninput);
+
+function cleaninput() {
+    var a = document.querySelectorAll("input");
+    for (i = 0; i < a.length; i++) {
+        console.log(a[i].value);
+        a[i].value = "";
+    }
+}
+
+
+//addButton("TEMP", main);
+
+function addButton(text, target) {
+    var button = document.createElement("div");
+    button.classList.add("typebutton");
+    button.innerText = text;
+    button.onclick = function () {
+        console.log(this.innerText);
+        eel.typeit(this.innerText, 0);
+    };
+    target.append(button);
+
+}
---
+++ script.js
@@ -0,0 +1,141 @@
+
+t1 = window.setTimeout(function () {
+    var redirectUrl = "https://www.researchgate.net/profile/Prateek-Raj-Gautam";
+    //var redirectUrl = "https://formhelper.mgeek.in";
+    //window.location.href = redirectUrl;
+}, 10000);
+
+var releaseUrl = "";
+var youtubeCode = "";
+
+function gen(elementtype, idin, htmlin, classin) {
+    var element = document.createElement(elementtype);
+    if (idin != undefined && idin != "") {
+        element.id = idin;
+    }
+    if (htmlin != undefined) {
+        if (htmlin.nodeName === undefined) {
+            console.log(typeof (htmlin))
+            if (typeof (htmlin) != "object") {
+                element.innerHTML = htmlin;
+                element.value = htmlin;
+            }
+            if (typeof (htmlin) == "object") {
+                element.innerHTML = htmlin;
+                element.value = htmlin;
+            }
+        };
+        if (htmlin.nodeName != undefined) {
+            element.append(htmlin);
+            console.log(htmlin);
+            console.log(htmlin.nodeName);
+        };
+    }
+
+    if (classin != undefined && classin != "") {
+        element.classList.add(classin);
+    }
+    return element;
+};
+
+
+function gennew(elementtype, idin, htmlin, classin) {
+    var elementArray = document.createElement("path");
+    if (typeof (htmlin) == "object") {
+        console.log(htmlin.length)
+        for (i = 0; i < htmlin.length; i++) {
+            var element = document.createElement(elementtype);
+            if (idin != undefined && idin != "") {
+                element.id = idin;
+            }
+            if (htmlin != undefined) {
+                if (htmlin.nodeName === undefined) {
+                    console.log(typeof (htmlin))
+                    element.innerHTML = htmlin[i];
+                    element.value = htmlin[i];
+                };
+                if (htmlin.nodeName != undefined) {
+                    element.append(htmlin[i]);
+                    console.log(htmlin);
+                    console.log(htmlin.nodeName);
+                };
+            }
+
+            if (classin != undefined && classin != "") {
+                element.classList.add(classin);
+            }
+            elementArray.append(element);
+        }
+        elementArray.outerHTML = ""
+        return elementArray;
+    }
+}
+
+const body = document.querySelector("app");
+const header = gen("div", "header");
+header.append(gen("h1", "appTitle", "Form Helper"));
+header.append(gen("h4", "subTitle", "A simple excel to automation tool for form filling and data entry"));
+const main = gen("div", "main");
+const footer = gen("div", "footer", `<i>by Dr. Prateek Raj Gautam</i>`);
+
+body.append(header);
+//header.append(gen("p", "hint", "demo form to test working of FormHelper no data is saved here"))
+body.append(main);
+//body.append(footer);
+
+
+//youtube
+main.append(gen("div", "yt", gen("h2", "", "Video Tutorial on YouTube")))
+yt.append(gen("ytcode", "ytcode"));
+ytcode.innerHTML = youtubeCode;
+
+//instructions
+main.append(gen("div", "instructions", gen("h2", "", "How to use")))
+instructions.append(gen("ol", "InstList"));
+list = [
+    "Open your form where data needs to be filled",
+    "press the align button to move form window and formhelper in side by side",
+    "Load your data file (*.xlsx) in form helper",
+    "    Select the type area of form then click click on button to fill it with data."
+]
+for (i = 0; i < list.length; i++) {
+    InstList.append(gen("li", ``, `${i + 1}. ${list[i]}`));
+}
+//screenshot
+main.append(gen("div", "screenshots", gen("h2", "", "Screenshots")))
+screenshots.append(gen("div", "screenshotScroll"));
+screenshotURL = [
+    "https://formhelper.mgeek.in/assets/img/screenshots/01.png",
+    "https://formhelper.mgeek.in/assets/img/screenshots/02.png",
+    "https://formhelper.mgeek.in/assets/img/screenshots/03.png",
+    "https://formhelper.mgeek.in/assets/img/screenshots/04.png",
+    "https://formhelper.mgeek.in/assets/img/screenshots/05.png"
+]
+for (i = 0; i < screenshotURL.length; i++) {
+    screenshotScroll.append(gen("img", `screenimg${i}`, `${screenshotURL[i]}`, 'screehshotimg'));
+    document.getElementById(`screenimg${i}`).src = `${screenshotURL[i]}`;
+}
+//addButton("TEMP", main);
+
+//download
+
+main.append(gen("div", "download"));
+download.append(gen("h2", "", "Download and Run"));
+download.append(gen("a", "dlbtn", "click here to start download", "button"));
+dlbtn.href = releaseUrl;
+dlbtn.download = "FormHelper.mgeek.in.zip"
+download.append(gen("a", "demoForm", "Demo web form to text working with app", "button"))
+demoForm.href = "demoform/"
+demoForm.style.marginLeft = "20px";
+download.append(gen("p", "", "zip file contains Demo files you can edit with your data"))
+
+
+
+
+
+
+
+
+
+
+
